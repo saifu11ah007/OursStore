8ad4315b246c2b84413b45b63057bd1541a556bf
saifu11ah007 <saifulah880@gmail.com>
2025-02-16 13:36:08 +0500

    Updated frontend and backend for Vercel deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import MainSlider from './components/MainSlider';
 import Products from './components/Products';
 import { Outlet } from 'react-router-dom'; // Import Outlet for nested routes
 
-import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
