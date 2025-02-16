01141a6aa5955139a3bcf1a5a62298888c01caf7
saifu11ah007 <saifulah880@gmail.com>
2025-02-16 15:40:56 +0500

    Added further  Vercel configuration

--- backend/api/mainRoutes.js
+++ backend/api/mainRoutes.js
@@ -4,7 +4,7 @@ import express from 'express';
 import productRoutes from '../api/routes/productRoutes.js';
 import userRoutes from '../api/routes/userRoutes.js';
 import orderRoutes from '../api/routes/orderRoutes.js';
-
+import paymentRoutes from '../api/routes/paymentRoutes.js';
 const app = express();
 
 // Body parser middleware
